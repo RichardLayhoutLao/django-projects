3b1af4509135e6950454de43c00a71f873f924f0
RIchard Layhout Lao <53919513+RichardLayhoutLao@users.noreply.github.com>
2023-10-26 05:28:54 -0400

    Add section 7

--- mysite/food/models.py
+++ mysite/food/models.py
@@ -5,6 +5,7 @@ class Item(models.Model):
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField(max_length=200)
     item_price = models.IntegerField()
+    item_image = models.CharField(max_length=500, default="https://dogtime.com/wp-content/uploads/sites/12/2011/01/GettyImages-653001154-e1691965000531.jpg")
 
 
     def __str__(self):
